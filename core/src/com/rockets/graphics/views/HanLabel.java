25de7593f24e26b9475344fc2ac116ea1df38195
davjhan <mr.davidjhan@gmail.com>
2017-05-26 21:55:52 -0700

    Introduced concept of fresh. reset is now fresh. fresh() means

--- core/src/com/rockets/graphics/views/HanLabel.java
+++ core/src/com/rockets/graphics/views/HanLabel.java
@@ -1,6 +1,5 @@
 package com.rockets.graphics.views;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Colors;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
